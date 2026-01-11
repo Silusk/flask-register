7ad8c666bec3f9390a9362b66239536c2aaf901c
Silus K <146929403+Silusk@users.noreply.github.com>
2026-01-10 23:29:28 -0800

    for removing sqlite senetence

--- auth.py
+++ auth.py
@@ -5,9 +5,7 @@ import os
 
 app = Flask(__name__)
 uri= os.environ.get('DATABASE_URL')
-if uri is None:
-    uri="sqlite://auth.db"
-if uri.startswith("postgres://"):
+if uri and uri.startswith("postgres://"):
     uri=uri.replace("postgres://","postgresql://",1)
 app.config["SQLALCHEMY_DATABASE_URI"]=uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
